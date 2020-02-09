702155162257c3718e044318a12d8e35bf7f566a
calbertuk <charlie.allombert@gmail.com>
2020-02-09 00:54:55 +0000

    added donate adress

--- src/components/OasisTestingWarningMessage.jsx
+++ src/components/OasisTestingWarningMessage.jsx
@@ -20,7 +20,9 @@ class OasisTestingWarningMessage extends PureComponent {
               </span>
               <span styleName="Text">
                 <div>
-                  OasisDex is never shutting down. Long live the decentralised web. Email: forkoasisdex@protonmail.ch
+                  Updated to new contract and DAI on 02.09.2020<br />
+                  OasisDex is never shutting down. Long live the decentralised web. Email: forkoasisdex@protonmail.ch<br />
+                  Please consider donating to 0x3496A7157968f851444AfD279d6568BC8cB086a1
                 </div>
               </span>
             </li>
